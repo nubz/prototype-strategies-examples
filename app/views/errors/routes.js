435e85bc2addd0db2193bf553881eae6d3bb7049
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2022-02-06 14:44:27 +0000

    update with links to github

--- app/views/errors/routes.js
+++ app/views/errors/routes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
-
+router.get('/', function (req, res) {
+  req.session.destroy()
+  res.render('errors/index')
+})
 router.use('/textInput', require('./textInput/routes'))
 module.exports = router
