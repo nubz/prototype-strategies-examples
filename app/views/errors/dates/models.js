ee3438274d9bf79465067ea7e925f56a7b3e4b6b
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2022-04-03 13:53:08 +0100

    show min and max date errors

--- app/views/errors/dates/models.js
+++ app/views/errors/dates/models.js
@@ -18,5 +18,93 @@ module.exports = {
         beforeToday: true
       }
     }
+  },
+  dateBeforeFixedDate: {
+    fields: {
+      bought: {
+        type: 'date',
+        name: 'the date you bought your shed',
+        min: '2022-01-01',
+        minDescription: 'the day the shed went on sale'
+      }
+    }
+  },
+  beforeFunctionDate: {
+    fields: {
+      bought: {
+        type: 'date',
+        name: 'the date you bought your shed',
+        min: data => {
+          const today = new Date()
+          const year = today.getFullYear() - 100
+          const month = today.getMonth() + 1
+          const day = today.getDate()
+          return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+        },
+        minDescription: 'one hundred years ago'
+      }
+    }
+  },
+  afterFixedDate: {
+    fields: {
+      bought: {
+        type: 'date',
+        name: 'the date you bought your shed',
+        max: '2022-02-01',
+        maxDescription: 'the last day the shed was on sale'
+      }
+    }
+  },
+  afterFunctionDate: {
+    fields: {
+      bought: {
+        type: 'date',
+        name: 'the date you bought your shed',
+        max: data => {
+          const today = new Date()
+          const year = today.getFullYear() - 1
+          const month = today.getMonth() + 1
+          const day = today.getDate()
+          return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+        },
+        maxDescription: 'one year ago'
+      }
+    }
+  },
+  betweenMinAndMaxFunctions: {
+    fields: {
+      bought: {
+        type: 'date',
+        name: 'the date you bought your shed',
+        min: data => {
+          const today = new Date()
+          const year = today.getFullYear() - 100
+          const month = today.getMonth() + 1
+          const day = today.getDate()
+          return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+        },
+        minDescription: 'one hundred years ago',
+        max: data => {
+          const today = new Date()
+          const year = today.getFullYear() - 1
+          const month = today.getMonth() + 1
+          const day = today.getDate()
+          return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+        },
+        maxDescription: 'one year ago'
+      }
+    }
+  },
+  betweenMinAndMax: {
+    fields: {
+      bought: {
+        type: 'date',
+        name: 'the date you bought your shed',
+        min: '2022-01-01',
+        minDescription: 'the date the shed went on sale',
+        max: '2022-02-01',
+        maxDescription: 'the last date the shed was on sale'
+      }
+    }
   }
 }
--- app/views/errors/dates/routes.js
+++ app/views/errors/dates/routes.js
@@ -66,4 +66,136 @@ router.post('/beforeToday', (req, res) => {
   }
 })
 
+router.get('/beforeFixedDate', (req, res) => {
+  res.render(templatePath, {
+    demoModel: demoModel(models.dateBeforeFixedDate), // this is NOT required for your templates - this is purely for demo display
+    dateErrorClasses: dateErrorClasses
+  })
+})
+
+router.post('/beforeFixedDate', (req, res) => {
+  const errors = getPageErrors(req.body, models.dateBeforeFixedDate)
+  if (errors.hasErrors) {
+    // re-render same template with errors
+    res.render(templatePath, {
+      errors, // we need to pass in the errors for use by the template
+      demoModel: demoModel(models.dateBeforeFixedDate), // this is NOT required for your templates - this is purely for demo display
+      dateErrorClasses: dateErrorClasses // NOT required for validating models - this is purely for demo display
+    })
+  } else {
+    // success, page is valid
+    res.redirect(homeRoute)
+  }
+})
+
+router.get('/beforeFunctionDate', (req, res) => {
+  res.render(templatePath, {
+    demoModel: demoModel(models.beforeFunctionDate), // this is NOT required for your templates - this is purely for demo display
+    dateErrorClasses: dateErrorClasses
+  })
+})
+
+router.post('/beforeFunctionDate', (req, res) => {
+  const errors = getPageErrors(req.body, models.beforeFunctionDate)
+  if (errors.hasErrors) {
+    // re-render same template with errors
+    res.render(templatePath, {
+      errors, // we need to pass in the errors for use by the template
+      demoModel: demoModel(models.beforeFunctionDate), // this is NOT required for your templates - this is purely for demo display
+      dateErrorClasses: dateErrorClasses // NOT required for validating models - this is purely for demo display
+    })
+  } else {
+    // success, page is valid
+    res.redirect(homeRoute)
+  }
+})
+
+router.get('/afterFixedDate', (req, res) => {
+  res.render(templatePath, {
+    demoModel: demoModel(models.afterFixedDate), // this is NOT required for your templates - this is purely for demo display
+    dateErrorClasses: dateErrorClasses
+  })
+})
+
+router.post('/afterFixedDate', (req, res) => {
+  const errors = getPageErrors(req.body, models.afterFixedDate)
+  if (errors.hasErrors) {
+    // re-render same template with errors
+    res.render(templatePath, {
+      errors, // we need to pass in the errors for use by the template
+      demoModel: demoModel(models.afterFixedDate), // this is NOT required for your templates - this is purely for demo display
+      dateErrorClasses: dateErrorClasses // NOT required for validating models - this is purely for demo display
+    })
+  } else {
+    // success, page is valid
+    res.redirect(homeRoute)
+  }
+})
+
+router.get('/afterFunctionDate', (req, res) => {
+  res.render(templatePath, {
+    demoModel: demoModel(models.afterFunctionDate), // this is NOT required for your templates - this is purely for demo display
+    dateErrorClasses: dateErrorClasses
+  })
+})
+
+router.post('/afterFunctionDate', (req, res) => {
+  const errors = getPageErrors(req.body, models.afterFunctionDate)
+  if (errors.hasErrors) {
+    // re-render same template with errors
+    res.render(templatePath, {
+      errors, // we need to pass in the errors for use by the template
+      demoModel: demoModel(models.afterFunctionDate), // this is NOT required for your templates - this is purely for demo display
+      dateErrorClasses: dateErrorClasses // NOT required for validating models - this is purely for demo display
+    })
+  } else {
+    // success, page is valid
+    res.redirect(homeRoute)
+  }
+})
+
+router.get('/betweenMinAndMaxFunctions', (req, res) => {
+  res.render(templatePath, {
+    demoModel: demoModel(models.betweenMinAndMaxFunctions), // this is NOT required for your templates - this is purely for demo display
+    dateErrorClasses: dateErrorClasses
+  })
+})
+
+router.post('/betweenMinAndMaxFunctions', (req, res) => {
+  const errors = getPageErrors(req.body, models.betweenMinAndMaxFunctions)
+  if (errors.hasErrors) {
+    // re-render same template with errors
+    res.render(templatePath, {
+      errors, // we need to pass in the errors for use by the template
+      demoModel: demoModel(models.betweenMinAndMaxFunctions), // this is NOT required for your templates - this is purely for demo display
+      dateErrorClasses: dateErrorClasses // NOT required for validating models - this is purely for demo display
+    })
+  } else {
+    // success, page is valid
+    res.redirect(homeRoute)
+  }
+})
+
+router.get('/betweenMinAndMax', (req, res) => {
+  res.render(templatePath, {
+    demoModel: demoModel(models.betweenMinAndMax), // this is NOT required for your templates - this is purely for demo display
+    dateErrorClasses: dateErrorClasses
+  })
+})
+
+router.post('/betweenMinAndMax', (req, res) => {
+  const errors = getPageErrors(req.body, models.betweenMinAndMax)
+  if (errors.hasErrors) {
+    // re-render same template with errors
+    res.render(templatePath, {
+      errors, // we need to pass in the errors for use by the template
+      demoModel: demoModel(models.betweenMinAndMax), // this is NOT required for your templates - this is purely for demo display
+      dateErrorClasses: dateErrorClasses // NOT required for validating models - this is purely for demo display
+    })
+  } else {
+    // success, page is valid
+    res.redirect(homeRoute)
+  }
+})
+
 module.exports = router
